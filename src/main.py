3d5227ebac2275b85877f0270d9b903d44b195c5
riduarte <riduarte88@gmail.com>
2020-09-01 10:38:15 +0000

    Creacion de tablas y funciones

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Cooker, Order
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,37 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/cooker', methods=['POST'])
+def handle_new_cooker():
+    new_cooker = request.get_json()
+    Cooker.addCooker(new_cooker)
+    return "You creater new cooker", 200 
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/cookers/<int:id>', methods=['GET'])
+def handle_cooker(id):
+    print("You have recived one cooker") 
+    return jsonify(Cooker.getUser(id)), 200
 
-    return jsonify(response_body), 200
+@app.route('/cookers', methods=['GET'])
+def handle_cookers():
+    print("You have recived all cookers") 
+    return jsonify(Cooker.getUsers()), 200
+
+@app.route('/order', methods=['POST'])
+def handle_new_order():
+    new_order = request.get_json()
+    Order.addOrder(new_order)
+    return "You creater new order", 200 
+
+@app.route('/orders', methods=['GET'])
+def handle_orders():
+    print("You have recived all orders") 
+    return jsonify(Order.getOrders()), 200
+
+@app.route('/orders/<int:id>', methods=['GET'])
+def handle_order(id):
+    print("You have recived one cooker") 
+    return jsonify(Order.getOrder(id)), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,97 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Cooker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nickname= db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    enterprise = db.Column(db.String(100), unique=False, nullable=False)
+    name= db.Column(db.String(80), unique=False, nullable=False)
+    last_name= db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    order = db.relationship('Order', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<Cooker {self.nickname}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "enterprise":self.enterprise,
+            "name":self.name,
+            "last_name":self.last_name
         }
+    def getUsers():
+        all_cookers= Cooker.query.all()
+        all_cookers = list(map(lambda x: x.serialize(), all_cookers))
+        
+        return all_cookers
+    
+    def getUser(cooker_id):
+        cooker= Cooker.query.filter_by(id = cooker_id).first()
+        cooker = cooker.serialize()
+       
+        return cooker
+    
+    def addCooker(cooker_data):
+        cooker_new= Cooker()
+        cooker_new.nickname = cooker_data["nickname"]
+        cooker_new.email = cooker_data["email"]
+        cooker_new.password = cooker_data["password"]
+        cooker_new.enterprise = cooker_data["enterprise"]
+        cooker_new.name = cooker_data["name"]
+        cooker_new.last_name = cooker_data["last_name"]
+        cooker_new.is_active= cooker_data["is_active"]
+
+        db.session.add(cooker_new)
+        db.session.commit()
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_code = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(80), unique=False, nullable=False)
+    time = db.Column(db.Integer, unique=False,nullable=False)
+    brand= db.Column(db.String(170), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    cooker_id= db.Column (db.Integer, db.ForeignKey ('cooker.id'))
+
+    def __repr__(self):
+        return f'<Order {self.order_code}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "status": self.status,
+            "time": self.time,
+            "brand":self.brand
+        }
+        
+    def addOrder(order_data):
+        order_new= Order()
+        order_new.order_code = order_data["order_code"]
+        order_new.status = order_data["status"]
+        order_new.time= order_data["time"]
+        order_new.brand = order_data["brand"]
+        order_new.is_active= order_data["is_active"]
+        
+        db.session.add(order_new)
+        db.session.commit()
+
+
+    def getOrders():
+        all_orders= Order.query.all()
+        all_orders = list(map(lambda x: x.serialize(), all_orders))
+        
+        return all_orders
+
+    def getOrder(order_id):
+        order= Order.query.filter_by(id = order_id).first()
+        order= order.serialize()
+        
+        return order
