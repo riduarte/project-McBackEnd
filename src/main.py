792dcc349e0a70a6d3af44f7f1c8b1dce0705a1c
JulioSau <tura_96@hotmail.com>
2020-09-11 14:21:28 +0000

    oauth, jimena revísalo porfa, y dime si así estaría bien

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
 import os
-from flask import Flask, request, jsonify, url_for
+import json
+
+#import requests
+from flask import Flask, redirect, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap, validation_global_cooker
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+
 from admin import setup_admin
-from models import db, Cooker, Called
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
+from models import db, User, UserOAuth
+from utils import APIException, generate_sitemap
+
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
 )
 
 #from models import Person
@@ -23,8 +37,27 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+#--------------------------------------------------------------------
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return UserOAuth.get(user_id)
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
 jwt = JWTManager(app)
+
+#---------------------------------------------------------------------------
+
 
 
 @app.route('/login', methods=['POST'])
@@ -121,6 +154,90 @@ def handle_edit_called(id):
 def handle_delete_called(id):
     deleted_called = Called.delete_called(id) 
     return deleted_called
+
+#------------------------------------------------------------------------------------------------
+@app.route("/login")
+def login():
+    #return request.base_url, 200
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url.replace('http://', 'https://') + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url.replace('http://', 'https://'),
+        redirect_url=request.base_url.replace('http://', 'https://') ,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in your db with the information provided
+    # by Google
+    user = UserOAuth(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not UserOAuth.get(unique_id):
+        UserOAuth.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("sitemap").replace('http://', 'https://'))
+
+#---------------------------------------------------------------------------------------------------
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -139,3 +139,35 @@ class Called(db.Model):
         else:
             return "Error",400
 
+class UserOAuth(db.Model, UserMixin):
+    id = db.Column(db.String(767), primary_key=True)
+    email = db.Column(db.String(767), unique=True, nullable=False)
+    name = db.Column(db.Text(), nullable=False)
+    profile_pic = db.Column(db.Text(), nullable=False)
+
+
+    def __init__(self, id_, name, email, profile_pic):
+        self.id = id_
+        self.name = name
+        self.email = email
+        self.profile_pic = profile_pic
+
+
+
+    @classmethod
+    def create(cls, id_, name, email, profile_pic):
+        user = cls(id_=id_, name=name, email=email, profile_pic=profile_pic)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def serialize (self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+        }
+
+    @classmethod
+    def get(cls, user_id):
+        return cls.query.filter_by(id=user_id).one_or_none()
